1bb997293c3bb80084fc965b221698e3f8d24214
wangdai <wangdai@chenjr.cc>
2014-11-07 17:28:21 +0800

    add upload and hash check backend, still draft

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from bottle import *
 from bottle import jinja2_template as template
-import os
 from config import *
+import os
+import hashlib
 
 #@route('/<name>')
 #def index(name):
@@ -13,24 +14,43 @@ def upload():
 
 @post('/')
 def do_upload():
-    #session = Session()
-    #try:
-    upload = request.files.get('upload')
-    barr = upload.file.read(1024)
-    print(barr)
-    print('SIZE=%d' % len(barr))
-        #name, ext = os.path.splitext(upload.filename)
-        #if ext not in('.png', '.jpg', '.gif'):
-        #    return 'File extension not allowed.'
-        #
-        #save_path = '/home/wangdai/www/'
-        #upload.save(save_path)
-    #except:
-    #    session.rollback()
-    #    raise
-    #finally:
-    #    session.close()
-    return 'OK'
+    session = Session()
+    try:
+        upload = request.files.get('upload')
+
+        # guess the file type
+        name, ext = os.path.splitext(upload.filename)
+        for cat in CATEGORY:
+            if ext in CATEGORY.get(cat):
+                break
+        if cat is None:
+            return template(BAD_REQUEST, "File extension '%s' not allowed" % ext)
+
+        # get the hash value for the first HASH_SIZE bytes
+        # and judge if same file exists
+        file_bytes = upload.file.read(HASH_SIZE)
+        md5_value = hashlib.md5(file_bytes).hexdigest()
+        same_item = session.query(Item).filter_by(hash_value=md5_value).first()
+        if same_item is not None:
+            return template("Your file '%s' already exists" % upload.filename, same_item.hash_name)
+
+        item = Item()
+        item.category = cat
+        item.hash_value = md5_value
+        item.hash_name = md5_value[8:24] + ext
+        item.origin_name = upload.filename
+
+        session.add(item)
+        session.commit()
+
+        save_path = '%s/%s' % (STATIC_PATH, cat)
+        upload.save(save_path)
+        return 'OK'
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 #run(server='gunicorn', host='localhost', port=8000)
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,18 +3,22 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import *
 from sqlalchemy.ext.declarative import declarative_base
 from config import *
+from datetime import *
 
 Base = declarative_base()
 
 class Item(Base):
     __tablename__ = 'item'
 
-    hash_value = Column(String(), primary_key=True)
-    orignal_name = Column(String)
+    hash_value = Column(String, primary_key=True)
+    origin_name = Column(String)
     hash_name = Column(String)
     category = Column(String)
     description = Column(String)
     upload_time = Column(TIMESTAMP)
+
+    def __init__(self):
+        self.upload_time = datetime.today()
 
 engine = create_engine('sqlite:///%s' % DB_NAME, echo=True)
 Base.metadata.create_all(engine)
