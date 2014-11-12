ce7e0bd56fab0b9698e8a6ddd38e68dd41fe4c04
wangdai <wangdai@chenjr.cc>
2014-11-12 16:39:58 +0800

    modify category suffix

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ FILE_ROOT = STATIC_PATH + '/dupload'
 
 CATEGORY = {
     'img'  : ('.jpg', '.png', '.gif'),
-    'doc'  : ('.txt', '.pdf', '.doc', '.docx', '.xml', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.tsv'),
+    'doc'  : ('.txt', '.pdf', '.doc', '.docx', '.xml', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.tsv', '.md'),
     'music': ('.mp3',),
-    'pack' : ('.zip', '.gz', '.bz2', '.tar', '.7z', '.md')
+    'pack' : ('.zip', '.gz', '.bz2', '.tar', '.7z', '.tgz', '.xz')
 }
 
 PAGE_SIZE = 10
